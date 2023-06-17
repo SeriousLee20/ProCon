6d5286fdd8a3d66696c59ab64cc34fe8739c50bc
SeriousLee20 <LHuiXin01@gmail.com>
2023-06-17 11:51:29 +0800

    fix input data binding, no validation

--- pages/profile.vue
+++ pages/profile.vue
@@ -3,23 +3,14 @@
     <Avatar />
   </div>
   <form
-    @submit="submitProfile"
+    @submit="updateProfile"
     class="flex flex-column gap-5 align-items-center justify-content-center mt-3"
   >
     <div>
       <span class="p-float-label">
-        <Pinputtext
-          id="name"
-          v-model="name"
-          type="text"
-          :class="{ 'p-invalid': errors.name }"
-          aria-describedby="text-error-name"
-        />
+        <Pinputtext id="name" v-model="name" type="text" required />
         <label for="name">Name</label>
       </span>
-      <small class="p-error" id="text-error-name">{{
-        errors.name || "&nbsp;"
-      }}</small>
     </div>
 
     <div>
@@ -29,30 +20,16 @@
           v-model="phone"
           placeholder="+60 10-12345678"
           mask="+99 99-99999999?9999"
-          :class="{ 'p-invalid': errors.phone }"
-          aria-describedby="text-error-phone"
         />
         <label for="phone">Contact Number</label>
       </span>
-      <small class="p-error" id="text-error-phone">{{
-        errors.phone || "&nbsp;"
-      }}</small>
     </div>
 
     <div>
       <span class="p-float-label">
-        <Pinputtext
-          id="email"
-          v-model="email"
-          type="email"
-          :class="{ 'p-invalid': errors.email }"
-          aria-describedby="text-error-email"
-        />
+        <Pinputtext id="email" v-model="email" type="email" disabled />
         <label for="email">Email</label>
       </span>
-      <small class="p-error" id="text-error-email">{{
-        errors.email || "&nbsp;"
-      }}</small>
     </div>
 
     <div>
@@ -63,14 +40,10 @@
           type="text"
           placeholder="00:00:00"
           mask="99:99:99"
-          :class="{ 'p-invalid': errors.starttime }"
-          aria-describedby="text-error-start-time"
+          required
         />
         <label for="starttime">Start Working Time</label>
       </span>
-      <small class="p-error" id="text-error-start-time">{{
-        errors.starttime || "&nbsp;"
-      }}</small>
     </div>
 
     <div>
@@ -81,14 +54,10 @@
           type="text"
           placeholder="23:59:59"
           mask="99:99:99"
-          :class="{ 'p-invalid': errors.endtime }"
-          aria-describedby="text-error-end-time"
+          required
         />
         <label for="endtime">End Working Time</label>
       </span>
-      <small class="p-error" id="text-error-end-time">{{
-        errors.endtime || "&nbsp;"
-      }}</small>
     </div>
     <div>
       <Pbutton type="submit" label="Save" />
@@ -117,82 +86,86 @@ console.log(user);
 // var starttime = user.start_working_hour;
 // var endtime = user.end_working_hour;
 
-var name = ref("");
-var email = ref("");
-var phone = ref("");
-var starttime = ref("");
-var endtime = ref("");
-
-const updatedInput = function (value, setVal) {
-  return computed({
-    set(newVal) {
-      value.value = setVal;
-    },
-  });
-};
+var name = ref(user.name);
+var email = ref(user.email);
+var phone = ref(user.contact_number);
+var starttime = ref(user.start_working_hour);
+var endtime = ref(user.end_working_hour);
 
 console.log(name, email, phone, starttime, endtime);
 
-const validateProfile = {
-  name(value) {
-    console.log(value);
-    if (!value) {
-      console.log(value);
-      return "Required";
-    }
-    return true;
-  },
-  phone(value) {
-    if (!value) {
-      return "Required";
-    }
-    return true;
-  },
-};
+// const validateProfile = {
+//   name(value) {
+//     console.log(value);
+//     if (!value) {
+//       console.log(value);
+//       return "Required";
+//     }
+//     return true;
+//   },
+//   phone(value) {
+//     if (!value) {
+//       return "Required";
+//     }
+//     return true;
+//   },
+// };
 
-const { useFieldModel, errors, handleSubmit } = useForm({
-  validationSchema: validateProfile,
-});
-console.log(errors);
-[name, phone, starttime, endtime] = useFieldModel([
-  "name",
-  "phone",
-  "starttime",
-  "endtime",
-]);
-const submitProfile = handleSubmit((values) => {
+// const { useFieldModel, errors, handleSubmit } = useForm({
+//   validationSchema: validateProfile,
+// });
+// console.log(errors);
+
+// [data.name, phone, starttime, endtime] = useFieldModel([
+//   "data.name",
+//   "phone",
+//   "starttime",
+//   "endtime",
+// ]);
+
+function updateProfile() {
+  // alert(JSON.stringify(values, null, 2));
+  alert(name.value + email.value);
   console.log(name, email, phone, starttime, endtime);
-  alert(JSON.stringify(values, null, 2));
   var userId = dstore.getUserId;
-  var name = values.name ? values.name : user.name;
-  var phone = values.phone ? values.phone : user.contact_number;
-  var email = values.email ? values.email : user.email;
-  var startTime = values.starttime ? values.starttime : user.start_working_hour;
-  var endTime = values.endtime ? values.endtime : user.end_working_hour;
+  var nname = name.value ? name.value : user.name;
+  var nphone = phone.value ? phone.value : user.contact_number;
+  var nemail = email.value ? email.value : user.email;
+  var startTime = starttime.value ? starttime.value : user.start_working_hour;
+  var endTime = endtime.value ? endtime.value : user.end_working_hour;
+
+  //   var userId = dstore.getUserId;
+  //   var nname = name.value ? name : user.name;
+  //   var nphone = phone ? phone : user.contact_number;
+  //   var nemail = email ? email : user.email;
+  //   var startTime = starttime ? starttime : user.start_working_hour;
+  //   var endTime = endtime ? endtime : user.end_working_hour;
   const newProfile = {
     id: userId,
-    name: name,
-    email: email,
-    contact_number: phone,
+    name: nname,
+    email: nemail,
+    contact_number: nphone,
     start_working_time: startTime,
     end_working_time: endTime,
     avatar_url: "",
   };
+
+  //   try {
   dstore.createUser(newProfile);
+  //   } catch (error) {
+  //     alert(error);
+  //   }
   console.log("updated", dstore.getUser);
-});
-
-export {
-
-    name: updatedInput(name, storeToRefs(dstore.user.name)),
-    phone: updatedInput(phone, storeToRefs(dstore.user.contact_number)),
-    email: updatedInput(email, storeToRefs(dstore.user.email)),
-    starttime: updatedInput(
-      starttime,
-      storeToRefs(dstore.user.start_working_hour)
-    ),
-    endtime: updatedInput(endtime, storeToRefs(dstore.user.end_working_hour)),
 }
+
+// (name = updatedInput(name, storeToRefs(dstore.user.name))),
+//   (phone = updatedInput(phone, storeToRefs(dstore.user.contact_number))),
+//   (email = updatedInput(email, storeToRefs(dstore.user.email))),
+//   (starttime = updatedInput(
+//     starttime,
+//     storeToRefs(dstore.user.start_working_hour)
+//   )),
+//   (endtime = updatedInput(endtime, storeToRefs(dstore.user.end_working_hour)));
 </script>
 
 <style lang="scss" scoped></style>
