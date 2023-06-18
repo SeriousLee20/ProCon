6550f912fc095c9c5a604ac8a3d243e04d37283b
SeriousLee20 <LHuiXin01@gmail.com>
2023-06-18 14:01:58 +0800

    adjusted minor styling

--- pages/event/[eventid].vue
+++ pages/event/[eventid].vue
@@ -1,13 +1,20 @@
 <template>
-  <form @submit="addAnnouncement()" v-if="openModal" class="fixed top-0 left-0 w-screen h-screen disabled-div" style="
+  <form
+    @submit="addAnnouncement()"
+    v-if="openModal"
+    class="fixed top-0 left-0 w-screen h-screen disabled-div"
+    style="
       background-color: rgba(0, 0, 0, 0.7);
       z-index: 10;
       display: flex;
       justify-content: center;
       align-items: center;
-    ">
-    <div class="bg-white" style="
-      min-width: 500px;
+    "
+  >
+    <div
+      class="bg-white"
+      style="
+        min-width: 500px;
         font-family: sans-serif;
         border-radius: 8px;
         display: flex;
@@ -18,155 +25,225 @@
         padding-bottom: 24px;
         padding-left: 32px;
         padding-right: 32px;
-      ">
-      <div style="padding-top: 20px; padding-bottom: 20px;">
+      "
+    >
+      <div style="padding-top: 20px; padding-bottom: 20px">
         Add Announcement
-      </div> <span class="p-float-label">
+      </div>
+      <span class="p-float-label">
         <Pinputtext id="title" v-model="title" type="text" required />
-        <label for="title" style="text-align: center;">Title</label>
+        <label for="title" style="text-align: center">Title</label>
       </span>
       <span class="p-float-label">
-        <Ptextarea id="description" v-model="description" type="text" required />
+        <Ptextarea
+          id="description"
+          v-model="description"
+          type="text"
+          required
+        />
         <label for="description" style="text-align: center">Description</label>
       </span>
-      <Pmultiselect v-model="selectedUsers" :options="groupedUsers" optionLabel="label" optionGroupLabel="label" filter
-        optionGroupChildren="items" display="chip" placeholder="Select users to notify" class="w-full md:w-20rem">
+      <Pmultiselect
+        v-model="selectedUsers"
+        :options="groupedUsers"
+        optionLabel="label"
+        optionGroupLabel="label"
+        filter
+        optionGroupChildren="items"
+        display="chip"
+        placeholder="Select users to notify"
+        class="w-full md:w-20rem"
+      >
         <template #optiongroup="slotProps">
           <div class="flex align-items-center">
             <div>{{ slotProps.option.label }}</div>
           </div>
         </template>
       </Pmultiselect>
-      <div style="display: flex; gap:10px; align-items: center;">
+      <div style="display: flex; gap: 10px; align-items: center">
         <Pbutton type="submit">Add</Pbutton>
         <Pbutton @click="closeModal()">Close</Pbutton>
       </div>
     </div>
   </form>
   <div>
-    <div style="
+    <div
+      style="
         display: grid;
         height: 100%;
         grid-template-columns: repeat(2, minmax(0, 1fr));
         gap: 2rem;
         padding: 20px;
-      ">
+      "
+    >
       <div>
         This is task list, but under construction
-        <iframe style="pointer-events: none; border: none;" width="500" height="400"
-          src="https://embed.lottiefiles.com/animation/145811"></iframe>
-
+        <iframe
+          style="pointer-events: none; border: none"
+          width="500"
+          height="400"
+          src="https://embed.lottiefiles.com/animation/145811"
+        ></iframe>
       </div>
 
-      <div style="
+      <div
+        style="
           display: grid;
           height: 100%;
           grid-template-rows: repeat(2, minmax(0, 1fr));
           grid-row: span 1 / span 1;
           gap: 1.25rem;
-        ">
+        "
+      >
         <div>
-          <iframe style="pointer-events: none; border: none;"
-            src="https://embed.lottiefiles.com/animation/145811"></iframe>
+          <iframe
+            style="pointer-events: none; border: none"
+            src="https://embed.lottiefiles.com/animation/145811"
+          ></iframe>
           This is my task list, but under construction
         </div>
-        <div style="
+        <div
+          style="
             display: flex;
             justify-content: center;
             align-items: center;
             height: 100%;
-          ">
-          <div style="
+          "
+        >
+          <div
+            style="
               border: 5px solid #bae8e8;
               filter: drop-shadow(4px 6px 4px rgba(39, 35, 67, 0.25));
               border-radius: 30px;
               width: 100%;
               background-color: white;
-            ">
-            <div style="
+            "
+          >
+            <div
+              style="
                 display: grid;
                 grid-template-columns: repeat(3, minmax(0, 1fr));
-              ">
-              <div style="grid-column: span 1 / span 1;"></div>
-              <div style="grid-column: span 1 / span 1;">
-                <h5 style="
+              "
+            >
+              <div style="grid-column: span 1 / span 1"></div>
+              <div style="grid-column: span 1 / span 1">
+                <h5
+                  style="
                     font-family: 'Montserrat';
                     font-style: normal;
                     font-weight: 700;
                     font-size: 15px;
                     text-align: center;
-                  ">
+                  "
+                >
                   Announcements
                 </h5>
               </div>
 
-              <div style="
+              <div
+                style="
                   grid-column: span 1 / span 1;
                   height: 100%;
                   padding: 15px;
-                ">
-                <div style="
+                "
+              >
+                <div
+                  style="
                     display: flex;
                     flex-direction: row;
                     width: 100%;
                     justify-content: end;
                     align-self: center;
-                  ">
-                  <Pbutton @click="openModalNow()" icon="pi pi-plus" rounded outlined aria-label="Filter" />
+                  "
+                >
+                  <Pbutton
+                    @click="openModalNow()"
+                    icon="pi pi-plus"
+                    rounded
+                    outlined
+                    aria-label="Filter"
+                  />
                 </div>
               </div>
             </div>
             <ClientOnly>
               <div v-if="filteredAnnouncements.length > 0">
+                <div
+                  style="
+                    padding-left: 8px;
+                    padding-right: 8px;
+                    padding-top: 12px;
+                    padding-bottom: 12px;
+                    overflow-y: scroll;
+                    height: 200px;
+                  "
+                >
+                  <div
+                    v-for="announcement in filteredAnnouncements"
+                    :key="announcement.id"
+                  >
+                    <div
+                      style="
+                        width: 100%;
+                        display: flex;
+                        align-items: center;
+                        justify-content: space-between;
+                      "
+                    >
+                      <div
+                        style="display: flex; gap: 12px; align-items: center"
+                      >
+                        <p
+                          style="
+                            font-family: 'Montserrat';
+                            font-style: normal;
+                            font-weight: 700;
+                            font-size: 12px;
+                            line-height: 15px;
+                            display: flex;
+                            align-items: center;
+                          "
+                        >
+                          {{ announcement.name ?? "" }}
+                        </p>
+                        <i
+                          class="pi pi-info-circle"
+                          v-tooltip.top="
+                            announcement.description ??
+                            'No description provided'
+                          "
+                          style="font-size: 1rem; color: #4a9292"
+                        ></i>
+                      </div>
 
-                <div v-for="announcement in filteredAnnouncements" :key="announcement.id" style="
-                padding-left: 8px;
-                padding-right: 8px;
-                padding-top: 12px;
-                padding-bottom: 12px;
-                overflow-y: scroll;
-                height: 200px;
-              ">
-                  <div style="
-                  width: 100%;
-                  display: flex;
-                  align-items: center;
-                  justify-content: space-between;
-                ">
-                    <div style="display: flex; gap:12px; align-items:center;">
-                      <p style="
-                    font-family: 'Montserrat';
-                    font-style: normal;
-                    font-weight: 700;
-                    font-size: 12px;
-                    line-height: 15px;
-                    display: flex;
-                    align-items: center;
-                  ">
-                        {{ announcement.name ?? '' }}
+                      <p
+                        style="
+                          font-family: 'Montserrat';
+                          font-style: normal;
+                          font-weight: 400;
+                          font-size: 12px;
+                          line-height: 15px;
+                          display: flex;
+                          align-items: center;
+                        "
+                      >
+                        {{ formatDate(announcement.creation_timestamp) ?? "" }}
                       </p>
-                      <i class="pi pi-info-circle" v-tooltip.top="announcement.description ?? 'No description provided'"
-                        style="font-size: 1rem; color:#4a9292"></i>
                     </div>
-
-                    <p style="
-                    font-family: 'Montserrat';
-                    font-style: normal;
-                    font-weight: 400;
-                    font-size: 12px;
-                    line-height: 15px;
-                    display: flex;
-                    align-items: center;
-                  ">
-                      {{ formatDate(announcement.creation_timestamp) ?? '' }}
-                    </p>
                   </div>
                 </div>
               </div>
-
-              <div v-if="announcements.length === 0 || !announcements"
-                style="display: flex; justify-content: center; align-items: center; height: 200px;">
-                <p style="
+              <div
+                v-if="announcements.length === 0 || !announcements"
+                style="
+                  display: flex;
+                  justify-content: center;
+                  align-items: center;
+                  height: 200px;
+                "
+              >
+                <p
+                  style="
                     font-family: 'Montserrat';
                     font-style: normal;
                     font-weight: 400;
@@ -174,7 +251,8 @@
                     line-height: 15px;
                     display: flex;
                     align-items: center;
-                  ">
+                  "
+                >
                   No Announcements!
                 </p>
               </div>
@@ -206,8 +284,7 @@ const openModal = ref(false);
 const title = ref(null);
 const description = ref(null);
 
-
-console.log(table[0].user_id)
+console.log(table[0].user_id);
 dstore.setSelectedProject("-1");
 
 // Create a single supabase client for interacting with your database
@@ -218,49 +295,49 @@ const supabase = createClient(
 const toast = useToast();
 
 let announcements = [];
-console.log(eventid)
+console.log(eventid);
 
 const { data: announcementData, error: announcementError } = await supabase
-  .from('announcement')
-  .select()
+  .from("announcement")
+  .select();
 
-let { data: eventMember, error: getEventError } = await supabase
-  .rpc('get_users_by_event_id', {
-    n_event_id: eventid
-  })
+let { data: eventMember, error: getEventError } = await supabase.rpc(
+  "get_users_by_event_id",
+  {
+    n_event_id: eventid,
+  }
+);
 
-if (getEventError) console.error(getEventError)
-else console.log(eventMember)
-
+if (getEventError) console.error(getEventError);
+else console.log(eventMember);
 
 announcements = announcementData;
 
-console.log(announcements)
-
+console.log(announcements);
 
 let filteredAnnouncements = [];
 
-
 for (let i = 0; i < announcements.length; i++) {
   const announcement = announcements[i];
-  if (announcement.event_id === eventid && announcement.receiver_ids.includes(table[0].user_id)) {
+  if (
+    announcement.event_id === eventid &&
+    announcement.receiver_ids.includes(table[0].user_id)
+  ) {
     filteredAnnouncements.push(announcement);
   }
-  console.log("announcement fetching")
-  console.log(announcement)
-  console.log(announcement.event_id)
-  console.log(eventid)
-  console.log(announcement.event_id === eventid)
+  console.log("announcement fetching");
+  console.log(announcement);
+  console.log(announcement.event_id);
+  console.log(eventid);
+  console.log(announcement.event_id === eventid);
 
-
-  console.log(announcement.receiver_ids)
-  console.log(table[0].user_id)
-  console.log(announcement.receiver_ids.includes(table[0].user_id))
+  console.log(announcement.receiver_ids);
+  console.log(table[0].user_id);
+  console.log(announcement.receiver_ids.includes(table[0].user_id));
 }
 
-
-console.log(typeof filteredAnnouncements[0])
-console.log(typeof eventMember)
+console.log(typeof filteredAnnouncements[0]);
+console.log(typeof eventMember);
 let userOptions = [];
 
 if (Array.isArray(eventMember)) {
@@ -268,39 +345,34 @@ if (Array.isArray(eventMember)) {
     label: user.name,
     value: user.id,
   }));
-}
-else {
+} else {
   userOptions = {
     label: eventMember.name,
-    value: eventMember.id
-  }
+    value: eventMember.id,
+  };
 }
-
-
 
 const selectedUsers = ref([]);
 const valueArr = ref([]);
 
-
 const groupedUsers = ref([
   {
-    label: 'All Users',
-    items: [userOptions]
-  }
+    label: "All Users",
+    items: [userOptions],
+  },
 ]);
-console.log(selectedUsers)
+console.log(selectedUsers);
 
 const formatDate = (dateString) => {
   const date = new Date(dateString);
-  const day = date.toLocaleString('default', { day: '2-digit' });
-  const month = date.toLocaleString('default', { month: '2-digit' });
+  const day = date.toLocaleString("default", { day: "2-digit" });
+  const month = date.toLocaleString("default", { month: "2-digit" });
   const year = date.getFullYear();
-  const hours = date.toLocaleString('default', { hour: '2-digit' });
-  const minutes = date.toLocaleString('default', { minutes: '2-digit' });
-  const ampm = hours >= 12 ? 'PM' : 'AM';
+  const hours = date.toLocaleString("default", { hour: "2-digit" });
+  const minutes = date.toLocaleString("default", { minutes: "2-digit" });
+  const ampm = hours >= 12 ? "PM" : "AM";
   return `${minutes} ${ampm}`;
 };
-
 
 watchEffect(() => {
   if (!useSupabaseUser().value) {
@@ -313,7 +385,6 @@ watchEffect(() => {
     }
 
     console.log(typeof valueArr.value);
-
   }
 });
 
@@ -324,15 +395,11 @@ async function addAnnouncement() {
     event_id: eventid,
     creator_id: table[0].user_id,
     creation_timestamp: new Date(),
-    receiver_ids: valueArr.value
-  }
+    receiver_ids: valueArr.value,
+  };
   console.log(payload);
   try {
-
-    const { data, error } = await supabase
-      .from('announcement')
-      .insert(payload);
-
+    const { data, error } = await supabase.from("announcement").insert(payload);
 
     toast.add({
       severity: "success",
@@ -346,8 +413,7 @@ async function addAnnouncement() {
   } catch (error) {
     console.log(error);
   }
-};
-
+}
 
 function closeModal() {
   openModal.value = false;
@@ -363,7 +429,6 @@ const logout = async () => {
 };
 
 console.log(useRoute());
-
 </script>
 
 <style lang="scss" scoped></style>
