656b07dc87b2ee0cdf352f1ba151293ffb8dc756
SeriousLee20 <LHuiXin01@gmail.com>
2023-12-19 22:22:22 +0800

    supbase url

--- lib/supabase-client.ts
+++ lib/supabase-client.ts
@@ -1,8 +1,14 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "@/types/supabase";
 
+const url =
+  process?.env?.NEXT_PUBLIC_SITE_URL ??
+  process?.env?.NEXT_PUBLIC_VERCEL_URL ??
+  "http://localhost:3000/";
+
 const supabase = createClient<Database>(
-  process.env.SUPABASE_URL as string,
+  url as string,
+
   process.env.SUPABASE_ANON_KEY as string
 );
 
