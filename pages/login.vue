fcb08908839a4e5a1e19ba7c335de46a8d88439b
SeriousLee20 <LHuiXin01@gmail.com>
2023-12-20 04:32:28 +0800

    login redirect

--- pages/login.vue
+++ pages/login.vue
@@ -50,7 +50,7 @@ const runtimeConfig = useRuntimeConfig();
 const getURL = () => {
   let url =
     // process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-    runtimeConfig?.public.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    // runtimeConfig?.public.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
     "http://localhost:3000/";
   console.log("url", url);
   // Make sure to include `https://` when not localhost.
@@ -66,7 +66,7 @@ const login = async () => {
   const { data, error } = await auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: "/",
+      redirectTo: "https://pro-con.vercel.app/",
     },
   });
 
