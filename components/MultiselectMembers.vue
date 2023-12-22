7ca4c8d020a79a622b1dd5ddf210c85feead6a15
SeriousLee20 <LHuiXin01@gmail.com>
2023-12-23 07:08:44 +0800

    Multiselect members component

---
+++ components/MultiselectMembers.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- <Pmultiselect
+    :options="groupedUsers"
+    optionLabel="username"
+    optionValue="user_id"
+    optionGroupLabel="department"
+    filter
+    optionGroupChildren="members"
+    display="chip"
+    placeholder="Assign Task to"
+    class="w-full"
+  >
+    <template #optiongroup="slotProps">
+      <div class="flex align-items-center">
+        <div>{{ slotProps.option.department }}</div>
+      </div>
+    </template>
+  </Pmultiselect> -->
+
+</template>
+
+<script setup>
+defineProps(["groupedUsers", "selectedUsers"]);
+
+</script>
+
+<style lang="scss" scoped></style>
