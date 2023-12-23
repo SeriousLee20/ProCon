16621f092e46ddb21ea287b4dac80c4bbef77a42
SeriousLee20 <LHuiXin01@gmail.com>
2023-12-23 14:38:31 +0800

    Merge pull request #19 from SeriousLee20/feat/chat
    dependencies

--- lib/supabase-client.ts
+++ lib/supabase-client.ts
@@ -1,9 +1,15 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "@/types/supabase";
 
+const url =
+  process?.env?.NEXT_PUBLIC_SITE_URL ??
+  process?.env?.NEXT_PUBLIC_VERCEL_URL ??
+  "http://localhost:3000/";
+
 const supabase = createClient<Database>(
-  process.env.SUPABASE_URL as string,
-  process.env.SUPABASE_ANON_KEY as string
+  url as string,
+
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 );
 
 export default supabase;
