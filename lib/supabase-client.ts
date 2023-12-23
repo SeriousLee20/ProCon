c5ca627608c676ee184ccab81d3514ea30dbfdd4
SeriousLee20 <LHuiXin01@gmail.com>
2023-12-23 14:43:37 +0800

    Revert "supbase url"
    This reverts commit 656b07dc87b2ee0cdf352f1ba151293ffb8dc756.

--- lib/supabase-client.ts
+++ lib/supabase-client.ts
@@ -1,15 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "@/types/supabase";
 
-const url =
-  process?.env?.NEXT_PUBLIC_SITE_URL ??
-  process?.env?.NEXT_PUBLIC_VERCEL_URL ??
-  "http://localhost:3000/";
-
 const supabase = createClient<Database>(
-  url as string,
-
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+  process.env.SUPABASE_URL as string,
+  process.env.SUPABASE_ANON_KEY as string
 );
 
 export default supabase;
